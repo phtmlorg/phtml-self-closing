219e2da74294bac6c637a670a2b3457f70c6e387
Jonathan Neal <jonathantneal@hotmail.com>
2019-02-19 13:16:47 -0500

    2.0.0

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import phtml from 'phtml';
 export default new phtml.Plugin('phtml-self-closing', opts => {
 	const ignore = [].concat(Object(opts).ignore || []);
 
-	return root => {
-		root.walk(node => {
-			const shouldUnclose = node.type === 'element' && node.isSelfClosing && !node.isVoid && !ignore.includes(node.name);
+	return {
+		Element(node) {
+			const shouldUnclose = node.isSelfClosing && !node.isVoid && !ignore.includes(node.name);
 
 			if (shouldUnclose) {
 				node.isSelfClosing = false;
+				node.isWithoutEndTag = false;
+
+				if (!node.isVoid) {
+					node.source.before = node.source.before.replace(/\//g, '');
+				}
 			}
-		});
+		}
 	};
 });
